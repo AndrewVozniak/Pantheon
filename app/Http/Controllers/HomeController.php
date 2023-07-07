a8df2373284c0bf228bd0c3a16b39d8033155d30
Andrew Vozniak <66873943+AndrewVozniak@users.noreply.github.com>
2023-07-08 01:12:52 +0300

    ..

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,20 +14,24 @@ class HomeController extends Controller
      */
     public function index(): void
     {
-//        $user = new User();
-//        $user->name = 'John Doe';
-//        $user->email = 'john@example.com';
-//        $user->another_one = '123123';
-//        $user->save();
-//
+        $user = new User();
+        $user->name = 'John Doe';
+        $user->email = 'john@example.com';
+        $user->another_one = '123123';
+        $user->save();
+
 //        $user = User::find(6);
 //        dd($user->name);
 
-//        $user = User::find(2);
-//        $user->name = 'John Dose';
-//        $user->save();
-//        $user->delete();
-//        dd($user->name);
+
+        dd(User::all());
+
+
+        $user = User::find(2);
+        $user->name = 'John Dose';
+        $user->save();
+        $user->delete();
+        dd($user->name);
 
 //        User::delete(2);
         render('index', ['name' => 'John', 'someArray' => ['one', 'two', 'three'], 'emptyArray' => [], 'title' => 'Home Page']);
--- routes/web.php
+++ routes/web.php
@@ -2,4 +2,5 @@
 
 use App\Pantheon\Router\Router;
 
-Router::get('/', [App\Http\Controllers\HomeController::class, 'index'])->setName('home')->setMiddleware('auth');
+Router::get('/', [App\Http\Controllers\HomeController::class, 'index'])->setMiddleware('auth');
+Router::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->setName('home')->setMiddleware('auth');
